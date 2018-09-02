1e0eecd62ad59e6d45a34630d2566f6d5f6e4af2
sunajiu <sunajiu@yeah.net>
2018-09-02 14:14:26 +0800

    commit initial download file

---
+++ download/download.py
@@ -0,0 +1,17 @@
+# encoding:utf-8
+
+import urllib
+
+class Download:
+	def download(url, num_retries=2):
+		print('Downloading', url)
+		try:
+			html = urllib.request.urlopen(url).read()
+		except (URLError, HTTPError, ContentTooShortError) as e:
+			print('Download error:', e.reason)
+			html = None
+			if num_retries > 0:
+				if hasattr(e, 'code') and 500 <= e.code < 600:
+					return download(url, num_retries-1)
+		return html
+
